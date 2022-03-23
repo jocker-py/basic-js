737edb8c8197241a159569d2454052847bafadcd
jocker-py <buladzislau@gmail.com>
2022-03-23 14:24:35 +0300

    feat: add solution for delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function deleteDigit(n) {
+    n = n.toString();
+    let result = [];
+    let arr = n.split("");
+    let item;
+    let prev;
+    let next;
+    for (let i in arr) {
+        if (i === 0) {
+            item = arr.slice(1);
+        } else if (i === arr.length - 1) {
+            item = arr.slice(0, -1);
+        } else {
+            prev = arr.slice(0, i);
+            next = arr.slice(i);
+            item = prev.concat(next.slice(1));
+        }
 
+        result.push(Number(item.join("")));
+    }
+
+    return Math.max.apply(null, result);
+}
 module.exports = {
-  deleteDigit
+    deleteDigit,
 };
