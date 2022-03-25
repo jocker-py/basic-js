3c99c8897077a6a35ca912fb5a8751fdfe4cdd4d
jocker-py <buladzislau@gmail.com>
2022-03-25 13:29:46 +0300

    feat: add solution for dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    let dreamTeam = "";
+    if (Array.isArray(members)) {
+        let result = members.filter((member) => typeof member === "string");
+        if (result) {
+            result = result.map((member) => member.trim().toUpperCase());
+            result.sort();
+            for (let i of result) {
+                dreamTeam += i[0];
+            }
+            return dreamTeam;
+        }
+    } else {
+        return false;
+    }
 }
-
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
