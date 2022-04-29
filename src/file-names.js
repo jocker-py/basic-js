50ec5ae88696ad8508adc70d1de672a77dad8cb3
jocker-py <buladzislau@gmail.com>
2022-04-29 20:21:24 +0300

    feat: add solution for file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function renameFiles(names) {
+  const newArr = {};
+  const result = [];
+  names.forEach(name => {
+      if(!newArr.hasOwnProperty(name)){
+          newArr[name] = 0;
+      } else {
+          newArr[name] += 1;
+          name += `(${newArr[name]})`;
+          newArr[name] = 0;
+      }
+      result.push(name);
+
+  });
+  return result
 }
 
 module.exports = {
