891b387972c560f3b2d58b06579a4526dedd1624
jocker-py <buladzislau@gmail.com>
2022-05-02 19:49:31 +0300

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  toCrypt(message, key, crypt = true) {
+    function getNumberFromCharCode(letter) {
+      if (97 <= letter.charCodeAt(0) && letter.charCodeAt(0) <= 122) {
+        return letter.charCodeAt(0) - 97;
+      }
+      return letter;
+    }
+
+    function getLetterFromCharCode(letter) {
+      if (Number.isInteger(letter)) {
+        return String.fromCharCode(letter + 97);
+      }
+      return letter;
+    }
+
+    let mix = [];
+    let keyIndex = 0;
+    let item;
+
+    message = message.toLowerCase().split("").map(getNumberFromCharCode);
+    key = key.toLowerCase().split("").map(getNumberFromCharCode);
+
+    for (let i = 0; i < message.length; i++) {
+      if (Number.isInteger(message[i])) {
+        if (keyIndex === key.length) {
+          keyIndex = 0;
+        }
+
+        if (crypt === true) {
+          item = message[i] + key[keyIndex];
+          if (item >= 26) {
+            item = Math.abs(item - 26);
+          }
+        } else if (crypt === false) {
+          item = message[i] - key[keyIndex];
+          if (item < 0) {
+            item = Math.abs(item + 26);
+          }
+        }
+
+        keyIndex++;
+      } else {
+        item = message[i];
+      }
+      mix.push(item);
+    }
+
+    mix = mix.map(getLetterFromCharCode);
+
+    if (!this.direction) {
+      mix.reverse();
+    }
+
+    return mix.join("").toUpperCase();
+  }
+
+  encrypt(message, key) {
+    if (message && key) {
+      return this.toCrypt(message, key);
+    } else {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+
+  decrypt(message, key) {
+    if (message && key) {
+      return this.toCrypt(message, key, false);
+    } else {
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
